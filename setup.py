64072974f1edf8fe3a575a5ba298603160c6a193
Mario Monroy <mj.monroyc22@gmail.com>
2025-02-27 01:27:08 -0600

    wip

--- setup.py
+++ setup.py
@@ -1,8 +1,8 @@
 # -*- coding: utf-8 -*-
 from setuptools import setup, find_packages
 
-with open("requirements.txt") as f:
-    install_requires = f.read().strip().split("\n")
+#with open("requirements.txt") as f:
+#    install_requires = f.read().strip().split("\n")
 
 # get version from __version__ variable in restaurant_management/__init__.py
 from restaurant_management import __version__ as version
